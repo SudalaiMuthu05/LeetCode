324ae39cb01121d4332286c0f339d5e9bde9425d
SUDALAI MUTHU S <146719354+SudalaiMuthu05@users.noreply.github.com>
2026-06-20 15:21:36 +0530

    Time: 8 ms (16.34%) | Memory: 44.4 MB (16.23%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.math.*;
+
+class Solution {
+    public String multiply(String num1, String num2) {
+        BigInteger a = new BigInteger(num1);
+        BigInteger b = new BigInteger(num2);
+
+        BigInteger c = a.multiply(b);
+
+        return c.toString();
+
+
+
+    }
+}
