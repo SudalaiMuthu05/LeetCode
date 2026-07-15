e38cb4f519e92b670f738a7a338a3462ea34fb09
SUDALAI MUTHU S <146719354+SudalaiMuthu05@users.noreply.github.com>
2026-07-15 13:20:43 +0530

    Time: 7 ms (12.79%) | Memory: 43.1 MB (6.12%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -5,12 +5,11 @@ class Solution {
     public int gcdOfOddEvenSums(int n) {
         int sumEven = 0;
         int sumOdd = 0;
-        for(int i=1;i<=n*2;i++){
-            if(i%2==0){
-                sumEven+=i; 
-            }
-            else{
-                sumOdd+=i;
+        for (int i = 1; i <= n * 2; i++) {
+            if (i % 2 == 0) {
+                sumEven += i;
+            } else {
+                sumOdd += i;
             }
         }
         String s1 = String.valueOf(sumEven);
