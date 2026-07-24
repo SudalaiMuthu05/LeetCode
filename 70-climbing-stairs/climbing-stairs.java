c7fc061452edcb6bba564ccb57b192bf8f139bd9
SUDALAI MUTHU S <146719354+SudalaiMuthu05@users.noreply.github.com>
2026-07-24 10:35:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (17.46%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        double sqrt5 = Math.sqrt(5);
+        double phi = (1 + sqrt5) / 2;
+
+        return (int) Math.round(Math.pow(phi, n + 1) / sqrt5);
+    }
+}
