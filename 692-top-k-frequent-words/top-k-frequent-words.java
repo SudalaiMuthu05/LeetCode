2634e138f67657cb6bc3b792b00ae58b50ed11c4
SUDALAI MUTHU S <146719354+SudalaiMuthu05@users.noreply.github.com>
2026-07-29 13:04:37 +0530

    Time: 7 ms (87.55%) | Memory: 46.5 MB (70.09%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+        List<Map.Entry<String, Integer>> arr_list = new ArrayList<>(map.entrySet());
+
+        Collections.sort(arr_list, (a, b) -> {
+            if (a.getValue().equals(b.getValue())) {
+                return a.getKey().compareTo(b.getKey());
+            }
+            return b.getValue() - a.getValue();
+        });
+
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < k; i++) {
+            result.add(arr_list.get(i).getKey());
+        }
+        return result;
+    }
+}
