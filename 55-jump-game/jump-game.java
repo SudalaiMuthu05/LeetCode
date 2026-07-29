e044bf17086c1b47986932bf4d52f65c87fe8df4
SUDALAI MUTHU S <146719354+SudalaiMuthu05@users.noreply.github.com>
2026-07-29 20:43:18 +0530

    Time: 2 ms (88.85%) | Memory: 47.7 MB (92.07%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,25 +1,28 @@
+// class Solution {
+//     public boolean canJump(int[] nums) {
+
+//         int step_left = nums[0];
+//         for (int i = 1; i < nums.length; i++) {
+//             step_left--;
+//             if (step_left < 0)
+//                 return false;
+//             if (i == nums.length - 1)
+//                 return true;
+//             if (step_left < nums[i]) {
+//                 step_left = nums[i];
+//             }
+//         }
+//         return true;
+//     }
+// }
 class Solution {
     public boolean canJump(int[] nums) {
-
-        int step_left = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            step_left--;
-            if (step_left < 0)
+        int reachable = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > reachable)
                 return false;
-            if (i == nums.length - 1)
-                return true;
-            if (step_left < nums[i]) {
-                step_left = nums[i];
-            }
+            reachable = Math.max(reachable, i + nums[i]);
         }
         return true;
     }
 }
-
-// int reachable = 0;
-// for (int i = 0; i < nums.length; i++) {
-//     if (i > reachable)
-//         return false;
-//     reachable = Math.max(reachable, i + nums[i]);
-// }
-// return true;
