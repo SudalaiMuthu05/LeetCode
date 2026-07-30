f244d2dc235062935eff4d4b96e2fceeeb46fa6a
SUDALAI MUTHU S <146719354+SudalaiMuthu05@users.noreply.github.com>
2026-07-30 13:22:28 +0530

    Time: 26 ms (8.33%) | Memory: 47.9 MB (24.67%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,82 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+        for(int x : nums){
+            set.add(x);
+        }
+        int sum = 0;
+        for(int x : set){
+            sum+=x;
+        }
+        int set_size = set.size()+1;
+        int missing_num = Math.abs(sum - (set_size*(set_size+1))/2);
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int x : nums){
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int x : map.keySet()){
+            if(map.get(x)>1){
+                al.add(x);
+            }
+        }
+
+        int[] res = new int[al.size()+1];
+        int index = 0;
+        for(int x : al){
+            res[index++] = x;
+        }
+        res[index] = missing_num;
+        return res;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // HashMap<Integer, Integer> map1 = new HashMap<>();
+        // HashMap<Integer, Integer> map2 = new HashMap<>();
+
+        // for (int x : nums) {
+        //     if (map1.containsKey(x)) {
+        //         map1.put(x, map1.get(x) + 1);
+        //     } else {
+        //         map1.put(x, 1);
+        //     }
+        // }
+        // for (int i = 1; i <= nums.length; i++) {
+        //     map2.put(i, map1.getOrDefault(i, 0));
+        // }
+
+        // ArrayList<Integer> lst = new ArrayList<>();
+        // for (int x : map2.keySet()) {
+        //     if (map2.get(x) > 1) {
+        //         lst.add(x);
+        //     }
+        //     if(map2.get(x) == 0){
+        //         lst.add(x);
+        //     }
+        // }
+        // int[] arr = new int[lst.size()];
+        // int index = 0;
+        // for (int x : lst) {
+        //     arr[index++] = x;
+        // }
+        // // Arrays.sort(arr);
+        // return arr;
+    }
+}
