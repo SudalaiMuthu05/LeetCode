1468da319aec63631c78264008ad66dbb6c00a86
SUDALAI MUTHU S <146719354+SudalaiMuthu05@users.noreply.github.com>
2026-07-30 14:06:44 +0530

    Time: 25 ms (8.59%) | Memory: 48 MB (12.47%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-
         Set<Integer> set = new HashSet<>();
         for (int x : nums) {
             set.add(x);
@@ -11,19 +10,16 @@ class Solution {
         }
         int set_size = set.size() + 1;
         int missing_num = Math.abs(sum - (set_size * (set_size + 1)) / 2);
-
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int x : nums) {
             map.put(x, map.getOrDefault(x, 0) + 1);
         }
-
         ArrayList<Integer> al = new ArrayList<>();
         for (int x : map.keySet()) {
             if (map.get(x) > 1) {
                 al.add(x);
             }
         }
-
         int[] res = new int[al.size() + 1];
         int index = 0;
         for (int x : al) {
