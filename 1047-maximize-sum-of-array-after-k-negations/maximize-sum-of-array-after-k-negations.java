967262cc340d3418ddd95cb47347b0f1be318a2b
SUDALAI MUTHU S <146719354+SudalaiMuthu05@users.noreply.github.com>
2026-08-04 09:19:18 +0530

    Time: 6 ms (81.38%) | Memory: 44.1 MB (91.70%) - LeetSync

--- 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.java
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.java
@@ -1,21 +1,38 @@
 class Solution {
     public int largestSumAfterKNegations(int[] nums, int k) {
 
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+        // int sum = 0;
+
+        // for (int i = 0; i < nums.length && k > 0; i++) {
+        //     if (nums[i] < 0) {
+        //         nums[i] *= -1;
+        //         k--;
+        //     }
+        // }
+        // Arrays.sort(nums);
+        // if (k % 2 != 0) {
+        //     nums[0] *= -1;
+        // }
+        // for (int x : nums) {
+        //     sum += x;
+        // }
+        // return sum;
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
         int sum = 0;
 
-        for (int i = 0; i < nums.length && k > 0; i++) {
-            if (nums[i] < 0) {
-                nums[i] *= -1;
-                k--;
-            }
-        }
-        Arrays.sort(nums);
-        if (k % 2 != 0) {
-            nums[0] *= -1;
-        }
         for (int x : nums) {
-            sum += x;
+            queue.offer(x);
+        }
+
+        while (k > 0) {
+            int min = queue.poll() * -1;
+            queue.offer(min);
+            k--;
+        }
+        while (!queue.isEmpty()) {
+            sum += queue.poll();
         }
         return sum;
     }
