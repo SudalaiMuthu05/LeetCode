2c654bd5ffca5ba9bcfbeecbf93c06001d38c07d
SUDALAI MUTHU S <146719354+SudalaiMuthu05@users.noreply.github.com>
2026-08-05 09:58:36 +0530

    Time: 5 ms (21.38%) | Memory: 47 MB (15.42%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String reverseVowels(String s) {
+
+        char[] sc = s.toCharArray();
+        StringBuffer normal = new StringBuffer();
+        StringBuffer vowels = new StringBuffer();
+
+        for (int i = 0; i < sc.length; i++) {
+            if (sc[i] == 'A' || sc[i] == 'E' || sc[i] == 'I' || sc[i] == 'O' || sc[i] == 'U'
+                    || sc[i] == 'a' || sc[i] == 'e' || sc[i] == 'i' || sc[i] == 'o' || sc[i] == 'u') {
+
+                normal.append('\0');
+                vowels.append(sc[i]);
+
+            } else {
+                normal.append(sc[i]);
+            }
+        }
+
+        char[] vow = vowels.reverse().toString().toCharArray();
+        char[] norm = normal.toString().toCharArray();
+
+        int index = 0;
+
+        for (int i = 0; i < norm.length; i++) {
+            if (norm[i] == '\0') {
+                norm[i] = vow[index++];
+            }
+        }
+
+        return new String(norm);
+    }
+}
