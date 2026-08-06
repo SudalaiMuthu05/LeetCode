0b5b15378c644d4d097007eebbcf269e07593703
SUDALAI MUTHU S <146719354+SudalaiMuthu05@users.noreply.github.com>
2026-08-06 17:29:30 +0530

    Time: 5 ms (2.24%) | Memory: 45.7 MB (12.75%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,19 +1,8 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean search(int[] nums, int target) {
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                return true;
-            }
-        }
-        return false;
-        // int start = nums[0];
-        // int end = nums[nums.length-1];
-
-        // while(start<end){
-
-        // }
+        return Arrays.binarySearch(nums, target) >= 0;
     }
-
 }
