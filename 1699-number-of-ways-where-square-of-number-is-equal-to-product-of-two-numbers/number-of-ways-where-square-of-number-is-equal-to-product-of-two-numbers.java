ea6d0df64db563f612fcefab7c5cd68d30a587af
SUDALAI MUTHU S <146719354+SudalaiMuthu05@users.noreply.github.com>
2026-08-27 22:10:56 +0530

    Time: 22 ms (81.40%) | Memory: 45.2 MB (69.77%) - LeetSync

--- 1699-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
+++ 1699-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
@@ -1,21 +1,39 @@
 class Solution {
     public int numTriplets(int[] nums1, int[] nums2) {
-        return countType(nums1, nums2) + countType(nums2, nums1);
+        return countTriplets(nums1, nums2) + countTriplets(nums2, nums1);
     }
-    private int countType(int[] arr1, int[] arr2) {
-        Map<Long, Integer> productCount = new HashMap<>();
-        
-        for (int j = 0; j < arr2.length; j++) {
-            for (int k = j + 1; k < arr2.length; k++) {
-                long prod = (long) arr2[j] * arr2[k];
-                productCount.put(prod, productCount.getOrDefault(prod, 0) + 1);
+
+    private int countTriplets(int[] arr1, int[] arr2) {
+        Map<Long, Integer> freq = new HashMap<>();
+        for (int num : arr2) {
+            freq.put((long) num, freq.getOrDefault((long) num, 0) + 1);
+        }
+
+        int count = 0;
+
+        for (int val : arr1) {
+            long target = (long) val * val;
+
+            for (Map.Entry<Long, Integer> entry : freq.entrySet()) {
+                long j = entry.getKey();
+                int countJ = entry.getValue();
+
+                if (target % j == 0) {
+                    long k = target / j;
+
+                    if (k < j) {
+                        continue;
+                    }
+
+                    if (k == j) {
+                        count += (int) ((long) countJ * (countJ - 1) / 2);
+                    } else if (freq.containsKey(k)) {
+                        count += countJ * freq.get(k);
+                    }
+                }
             }
         }
-        int total = 0;
-        for (int val : arr1) {
-            long square = (long) val * val;
-            total += productCount.getOrDefault(square, 0);
-        }
-        return total;
+
+        return count;
     }
 }
